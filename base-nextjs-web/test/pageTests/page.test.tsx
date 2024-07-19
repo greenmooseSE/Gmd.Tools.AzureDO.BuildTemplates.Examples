f93b6f88c27f2738e6dd28b8e073116074cfe1bf
GreenMoose <juggernaut78@gmail.com>
2024-07-19 15:04:54 +0200

    AB#351 Fix ts compile errors and add TsCommon.Utils module [cdCiForNextJsApp]

--- base-nextjs-web/test/pageTests/page.test.tsx
+++ base-nextjs-web/test/pageTests/page.test.tsx
@@ -1,6 +1,7 @@
 ﻿import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Home from "@/app/page";
+import Ensure from "../../submodules/Gmd.TsCommon.Utils/src/ensure";
 
 jest.mock('next/config', () => () => ({
     publicRuntimeConfig: {
@@ -24,15 +25,16 @@ describe('<Home />', () => {
     })
 
     test('clicking on image expands it', async () => {
-        var res = await render(<Home/>);
+        const res = await render(<Home/>);
         const imageAlt: HTMLElement = screen.getByAltText('Rounded avatar');
-        const parentDiv = imageAlt.parentElement;
+        const parentDiv: HTMLElement = Ensure.notNull(imageAlt.parentElement);
+        expect(parentDiv).not.toBeNull();
         expect(parentDiv.className).toContain("min-h-20");
         expect(parentDiv.className).not.toContain("h-screen");
         await userEvent.click(imageAlt as Element);
 
-        var imageAlt2 = await screen.findByAltText('Rounded avatar');
-        var parentDiv2 = imageAlt2.parentElement;
+        const imageAlt2 = await screen.findByAltText('Rounded avatar');
+        const parentDiv2 = Ensure.notNull(imageAlt2.parentElement);
         expect(parentDiv2.className).toContain("h-screen");
         // expect(imageAlt.className).toContain("h-screen");
     });
